f634b63e6a366ad01df5c0fd8c8c432fe4246376
root <root@testz.p.zitta.fr>
2014-06-10 14:33:00 +0200

    add options to command

--- bin/berkshelf-store.rb
+++ bin/berkshelf-store.rb
@@ -1,6 +1,51 @@
 #!/usr/bin/env ruby
 $:.push File.expand_path("../../lib", __FILE__)
 
+require 'optparse'
 require 'berkshelf-store'
 
-BerkshelfStore::Webservice.run!({datadir:'/var/lib/berkshelf-store/cookbooks', tmpdir:'/var/tmp/berkshelf-store'})
+options = {
+        datadir:"./datadir",
+        tmpdir:"./tmpdir",
+        bind:"127.0.0.1",
+        port:"80",
+        logger_type:"syslog",
+        logger_conf:"berkshelf-store.daemon"
+}
+
+OptionParser.new do |opts|
+  opts.banner = "Usage: berkshelf-store.rb [options]"
+
+  opts.on("-D", "--datadir DIRECTORY", "Data directory (default: #{options[:datadir]})") do |d|
+    options[:datadir] = d
+  end
+
+  opts.on("-T", "--tmpdir DIRECTORY", "Tmp directory (default: #{options[:tmpdir]})") do |t|
+    options[:tmpdir] = t
+  end
+
+  opts.on("-b", "--bind IP", "bind IP (default: #{options[:bind]})") do |ip|
+    options[:bind] = ip
+  end
+
+  opts.on("-p", "--port PORT", "port to listen on (default: #{options[:port]})") do |p|
+    options[:port] = p
+  end
+ 
+  opts.on("-l", "--logger TYPE", "file, syslog, stderr (default: #{options[:logger_type]})") do |l|
+    options[:logger_type] = l
+  end
+  
+  opts.on("-L", "--logger-conf TYPE", "file: filename, syslog: program.facility, stderr n/a (default: #{options[:logger_conf]})") do |lc|
+    options[:logger_conf] = lc
+  end
+end.parse!
+
+p options
+
+#The only way I found to pass options to configure
+ENV['LOGGER_TYPE'] ||= options.delete(:logger_type)
+ENV['LOGGER_CONF'] ||= options.delete(:logger_conf)
+
+#remaining options will be available as settings
+BerkshelfStore::Webservice.run!(options)
--- lib/berkshelf-store/webservice.rb
+++ lib/berkshelf-store/webservice.rb
@@ -1,15 +1,41 @@
 require 'sinatra/base'
 require 'sinatra/json'
+require 'syslog-logger'
+require 'logger'
+
 #require 'berkshelf-store'
+::Logger.class_eval { alias :write :'<<' }
+::Logger::Syslog.class_eval { alias :write :'<<' }
 
 module BerkshelfStore
   class Webservice < Sinatra::Base
     helpers Sinatra::JSON
 
+    helpers do
+      def logger
+        settings.logger
+      end
+    end
+
+    configure do
+      logger_type = ENV['LOGGER_TYPE'].to_s
+      logger_conf = ENV['LOGGER_CONF'].to_s
+      if logger_type == "file"
+        logger = Logger.new(File.open("#{logger_conf}", 'a'))
+      elsif logger_type == "stderr"
+        logger = Logger.new(STDERR)
+      elsif logger_type == "syslog"
+        programname = logger_conf.split(".")[0]
+        facility = eval("Syslog::LOG_#{logger_conf.to_s.split(".")[1].upcase}")
+        logger = Logger::Syslog.new(programname, facility)
+      end
+      use ::Rack::CommonLogger, logger
+      set :logger, logger
+    end
     set :prefix, "/"
 
     get "/ping" do
-      "It's alive!!"
+      logger.info 'ping'
     end
 
     get "/v1/universe" do
